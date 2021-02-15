06ade2b9e2229369935152cf26e0cb3ee9b9a3f9
root <soumojyotiganguly270@gmail.com>
2021-02-15 13:39:12 +0530

    import library instatag

--- app/src/main/java/com/example/textinimage/MainActivity.java
+++ app/src/main/java/com/example/textinimage/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.textinimage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.harsh.instatag.InstaTag;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        InstaTag instaTag = findViewById(R.id.insta_tag);
+
     }
 }
